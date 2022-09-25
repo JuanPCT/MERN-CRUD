3f6f6f709bcc208dda076d4ab932f116e206091d
JuanPCT <89850618+JuanPCT@users.noreply.github.com>
2022-09-25 11:45:49 -0500

    creación de componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import './App.css';
+
+import Navigation from './Components/Navigation';
+import CreateUser from './Components/CreateUser';
+import CreateNote from './Components/CreateNote';
+import NotesList from './Components/NotesList';
 
 function App() {
   return (
     <div>
+      <Navigation/>
+
+
       Hola Mundo
     </div>
   );
